3ddbc42a4a32619b9fa5394c316e6c56bbec18d1
CalvinWilkinson <kinsondigital@gmail.com>
2026-02-10 21:47:47 +0000

    chore: create test helper methods

--- Testing/VelaptorAsepriteTests/Helpers/ExtensionMethods.cs
+++ Testing/VelaptorAsepriteTests/Helpers/ExtensionMethods.cs
@@ -29,4 +29,36 @@ public static class ExtensionMethods
                 ms),
         };
     }
+
+    /// <summary>
+    /// Generates a random number between the <paramref name="min"/> and <paramref name="max"/> values.
+    /// </summary>
+    /// <param name="value">The random number generator.</param>
+    /// <param name="min">The minimum value.</param>
+    /// <param name="max">The maximum value.</param>
+    /// <returns>The random result.</returns>
+    public static float NextRange(this Random value, float min, float max) => (value.NextSingle() * (max - min)) + min;
+
+    /// <summary>
+    /// Runs the given <paramref name="action"/> until the <paramref name="predicate"/> returns true.
+    /// </summary>
+    /// <param name="action">The action to execute.</param>
+    /// <param name="predicate">The predicate to return true to stop executing the <paramref name="action"/>.</param>
+    /// <param name="totalAttempts">The total number of attempts to execute the action until an exception is thrown.</param>
+    /// <exception cref="Exception">Thrown if the total number of attempts has been exceeded.</exception>
+    public static void RunUntil(this Action action, Func<bool> predicate, int totalAttempts = 1_000)
+    {
+        var currentAttempt = 0;
+
+        while (!predicate())
+        {
+            action();
+            currentAttempt++;
+
+            if (currentAttempt >= totalAttempts)
+            {
+                throw new Exception($"The action did not complete within '{totalAttempts}' executions.");
+            }
+        }
+    }
 }
